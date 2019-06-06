88c47e24647b79219dd2a167fc42474d5a826d91
Kevin <kmclaughlin42@qub.ac.uk>
2019-06-06 11:18:34 +0100

    Updated config to reflect interrupt driven stepper changes

--- firmware/Config.h
+++ firmware/Config.h
@@ -3,49 +3,50 @@
 
 // Joint 0 - Shoulder Yaw 
 #define J0_STEPS_PER_DEG 44
-#define J0_DELAY 500
-#define J0_START_DELAY 10000
-#define J0_PROFILE_STEPS 300
+#define J0_SPEED 20
+#define J0_MIN_SPEED 5
+#define J0_ACCEL_RATE 100
 #define J0_MAX_ROT_DEG 180
 
 //Joint 1 - Shoulder Pitch
 #define J1_STEPS_PER_DEG 60
-#define J1_DELAY 600
-#define J1_START_DELAY 10000
-#define J1_PROFILE_STEPS 300
+#define J1_SPEED 10
+#define J1_MIN_SPEED 5
+#define J1_ACCEL_RATE 100
 #define J1_MAX_ROT_DEG 180
 
 //Joint 2 Elbow Pitch
 #define J2_STEPS_PER_DEG 60
-#define J2_DELAY 600
-#define J2_START_DELAY 10000
-#define J2_PROFILE_STEPS 300
+#define J2_SPEED 12
+#define J2_MIN_SPEED 5
+#define J2_ACCEL_RATE 100
 #define J2_MAX_ROT_DEG 180
 
 // Joint 3 Elbow Roll
 #define J3_STEPS_PER_DEG 44
-#define J3_DELAY 300
-#define J3_START_DELAY 10000
-#define J3_PROFILE_STEPS 300
+#define J3_SPEED 10
+#define J3_MIN_SPEED 5
+#define J3_ACCEL_RATE 100
 #define J3_MAX_ROT_DEG 180
 
 //Joint 4 Wrist Pitch
 #define J4_STEPS_PER_DEG 44
-#define J4_DELAY 300
-#define J4_START_DELAY 10000
-#define J4_PROFILE_STEPS 300
+#define J4_SPEED 10
+#define J4_MIN_SPEED 5
+#define J4_ACCEL_RATE 100
 #define J4_MAX_ROT_DEG 180
 
 //Joint 5 Wrist Roll
 #define J5_STEPS_PER_DEG 44
-#define J5_DELAY 300
-#define J5_START_DELAY 10000
-#define J5_PROFILE_STEPS 300
+#define J5_SPEED 10
+#define J5_MIN_SPEED 5
+#define J5_ACCEL_RATE 100
 #define J5_MAX_ROT_DEG 180
 
 //other constants
 #define TOTAL_JOINTS 4
 #define BAUD_RATE 115200
 #define INST_ARRAY_LEN 20 //length of instruction array
+#define INTERRUPT_TIME 100
 
 #endif

388bb08f1bc8f8219c5c77364b80de0646cc081e
Ryan McCartney <rmccartney856@gmail.com>
2019-06-11 11:44:34 +0100

    Folder Update

---
+++ firmware/pins.h
@@ -0,0 +1,43 @@
+#ifndef PINS_H
+#define PINS_H
+
+// Joint 0 - Shoulder Yaw 
+#define J0_STEP 2
+#define J0_DIR 22
+#define J0_ENABLE 23
+#define J0_L_SWITCH 40
+
+//Joint 1 - Shoulder Pitch
+#define J1_STEP 3
+#define J1_DIR 24
+#define J1_ENABLE 25
+#define J1_L_SWITCH 41
+
+//Joint 2 Elbow Pitch
+#define J2_STEP 4
+#define J2_DIR 26
+#define J2_ENABLE 27
+#define J2_L_SWITCH 42
+
+// Joint 3 Elbow Roll
+#define J3_STEP 5
+#define J3_DIR 28
+#define J3_ENABLE 29
+#define J3_L_SWITCH 43
+
+//Joint 4 Wrist Pitch
+#define J4_STEP 6
+#define J4_DIR 30
+#define J4_ENABLE 31
+#define J4_L_SWITCH 44
+
+//Joint 5 Wrist Roll
+#define J5_STEP 7
+#define J5_DIR 32
+#define J5_ENABLE 33
+#define J5_L_SWITCH 45
+
+//End Effector Connection
+#define HAND_PIN 13
+
+#endif

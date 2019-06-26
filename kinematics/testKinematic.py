5511454c4a6138699f746e0f2e0f4bd0221308ed
makerspacequb <makerspace.qub@gmail.com>
2019-06-26 15:56:47 +0100

    File Renaming

--- kinematics/testKinematic.py
+++ kinematics/testKinematic.py
@@ -1,6 +1,5 @@
 #script to demonstrate how to properly to interact with the kinematicSolver
 
-
 from kinematicSolver import Kinematic
 
 #instantiate kinematic object
@@ -9,8 +8,6 @@ kin = Kinematic()
 #input joint positions (work out why there is angles added and subtracted from 2 joints? caution when testing)
 # [J0,J1,J2,J3,J4,J5]
 joint_positions = [0.01,-90,(90-90),0.01,0.01,(0.01+180)]
-
-
 
 print("Running Forward Kinematics")
 #perform forward kinematics and extract end effector pose

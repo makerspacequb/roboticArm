0791bac8bb6996d1d8bb347b9313a8127bb1f762
QLab Makerspace <makersapce.qub@gmail.com>
2019-07-08 12:09:05 +0100

    Seperated Demos from Tests

--- tests/testInverseKinematic.py
+++ tests/testInverseKinematic.py
@@ -1,4 +1,4 @@
-#NAME: testKinematic.py
+#NAME: testForwardKinematic.py
 #DATE: 05/06/2019
 #AUTH: QLab Makerspace
 #DESC: Script to demonstrate how to properly to interact with the Kinematic Class
@@ -7,21 +7,27 @@
 import sys
 sys.path.append('control/')
 from kinematics import Kinematic
-import time
 
 #instantiate kinematic object
 kin = Kinematic()
 
+"""
 #input joint positions (work out why there is angles added and subtracted from 2 joints? caution when testing)
+# [J0,J1,J2,J3,J4,J5]
 joint_positions = [0.01,-90,(90-90),0.01,0.01,(0.01+180)]
-#[J0,J1,J2,J3,J4,J5]
 
-print("INFO: Running Forward Kinematics")
-startTime = time.time()
-
+print("Running Forward Kinematics")
 #perform forward kinematics and extract end effector pose
 pose = kin.forwardKinematics(joint_positions)
 #pose =  [X,Y,Z,Yaw,Pitch,Roll]
+print(pose)
+print("Task complete")
+"""
 
-print("INFO: Pose is '"+str(pose)+"'.")
-print("INFO: Forward Kinematic Test complete. Processing took "+str(round(time.time()-startTime,2))+" seconds.")
+#insert a desired pose for inverse kinematics
+desired_pose = [500, 150, 474.7636732, -20, 50, -90.00999825]
+
+#inverse kinematics not finished
+Joints = kin.inverseKinematics(desired_pose)
+
+print(Joints)
